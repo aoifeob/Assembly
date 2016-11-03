34f97fc8415d77ebe929c52c4a8d2d33ebf8903d
obriea21 <obriea21@tcd.ie>
2016-11-03 01:04:51 +0000

    Update Polynomial.s

--- Polynomial.s
+++ Polynomial.s
@@ -4,22 +4,22 @@
 
 start
 	; let r1 store x
-  mul r0,r1,r1 ; r0 <- x*x
-  mul r0,r1,r0 ;r0 <- x*x*x
-  mul r0,r1,r0 ;r0 <- x*x*x*x (ie x to the fourth power)
+  mul r0,r1,r1 ; r0 <- x^2
+  mul r0,r1,r0 ;r0 <- x^3
+  mul r0,r1,r0 ;r0 <- x^4
   LDR r2,=6	; temp r2 <- 6
-  mul r0,r2,r0 ;r0 <- 6*x*x*x*x
+  mul r0,r2,r0 ;r0 <- 6*x^4
 
-  mul r2,r1,r1 ; r2<- x*x
+  mul r2,r1,r1 ; r2<- x^2
   LDR r3,=4 ; temp r3 <- 4
-  mul r2,r3,r2 ; r2<-4*x*x
+  mul r2,r3,r2 ; r2<-4*x^2
 
-  SUB r0,r0,r2; r0<- (6*x*x*x*x)-(4*x*x)
+  SUB r0,r0,r2; r0<- (6*x^4)-(4*x^2)
 
   LDR r2,=6 ;r2<- 6
   mul r2,r1,r2; r2<- x*6
 
-  ADD r0,r0,r2 ;r0<- [(6*x*x*x*x)-(4*x*x)]+6x
+  ADD r0,r0,r2 ;r0<- [(6*x^4)-(4*x^2)]+6x
 
 
 	
