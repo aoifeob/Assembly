57eb19c653647b20376f38cbed38d03592ebaeb8
obriea21 <obriea21@tcd.ie>
2016-12-07 11:12:19 +0000

    Update SetAndClearedCount.s

--- SetAndClearedCount.s
+++ SetAndClearedCount.s
@@ -3,7 +3,7 @@
 	EXPORT	start
 
 start
-	  ; r0 stores 9-bit number
+	  	; r0 stores 9-bit number
 		; r1 stores set bit count (ie 1s)
 		; r2 stores cleared bit count (ie 0s)
 
@@ -13,22 +13,31 @@ start
 
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, first shift 
 		ADC r1, r1, #0  ; r1=r1+ Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, second shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, third shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, fourth shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, fifth shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, sixth shift
-	  ADC r1, r1, #0	; r1 = r1 + Carry-flag
+	  	ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, seventh shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, eighth shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		MOVS r0, r0, LSR #1 ; r0=r0>>1, ninth shift
 		ADC r1, r1, #0	; r1 = r1 + Carry-flag
+		
 		SUB r2, r2, r1  ; r2=r2-r1
 
 stop	B	stop
