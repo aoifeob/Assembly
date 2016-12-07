db85ad884afaa5c520fe4c7452557363e0981155
obriea21 <obriea21@tcd.ie>
2016-12-07 16:52:36 +0000

    Create Reversal.s

---
+++ Reversal.s
@@ -0,0 +1,19 @@
+start
+	  LDR r0, =2_1100 ;test value
+		LDR r1, =32	       ;counter
+		LDR	r2, =2_0000		   ;store reversed bits
+ 	
+while	CMP r1, #0		   ; while (r1 != 0)
+		BEQ endwhile	   ; {
+	  MOVS r0, r0,LSL #1 ; r0=r0<<1, Carry-flag=r0[MSB]
+		RRX r2, r2         ; r2=r2>>1, r2[MSB]=Carry-flag
+		SUB r1, r1, #1	   ; r1=r1-1
+		B while            ; }
+endwhile	
+
+		MOV r0, r2		   ; r0=r2
+
+
+stop	B	stop
+
+	END	
